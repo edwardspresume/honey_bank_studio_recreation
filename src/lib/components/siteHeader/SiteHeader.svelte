33c9776bee3797fd4b6d320e5ba1382279ee0ff2
Edwards Presume <edwardspresume@gmail.com>
2023-10-26 07:18:02 -0400

    fix:  changed to fixed so that the header is out of regular layout flow

--- src/lib/components/siteHeader/SiteHeader.svelte
+++ src/lib/components/siteHeader/SiteHeader.svelte
@@ -14,7 +14,7 @@
 
 <svelte:window bind:scrollY />
 
-<header class="sticky top-0 z-50 duration-300 transition-colors {bgPrimary ? 'bg-primary' : ''}">
+<header class="fixed w-full top-0 z-50 duration-300 transition-colors {bgPrimary ? 'bg-primary' : ''}">
 	<div class="container flex justify-between p-4">
 		<a href={RoutePaths.HOMEPAGE} class="b">
 			<img src={logoSRC} alt="Honeybank Studios" class="w-56 sm:w-80" />
