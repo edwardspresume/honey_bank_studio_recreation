4fe11ffb8bd14cba2751c3c44ccf2d3af1ddbf29
Edwards Presume <edwardspresume@gmail.com>
2023-10-26 07:30:45 -0400

    feat: created contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import PageHeader from '$components/PageHeader.svelte';
+    import type { PageData } from './$types';
+
+    export let data: PageData;
+</script>
+
+<PageHeader>
+    <h1>Contact</h1>
+</PageHeader>
