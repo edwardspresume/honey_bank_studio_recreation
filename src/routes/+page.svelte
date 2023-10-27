8378019922b9f6f31668bd8dcee520f2098869d4
Edwards Presume <edwardspresume@gmail.com>
2023-10-27 14:52:56 -0400

    feat(+page.svelte): added page section content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,119 +1,68 @@
 <script>
+	import BackgroundSection from '$components/BackgroundSection.svelte';
 	import H1 from '$components/H1.svelte';
+	import H2 from '$components/H2.svelte';
 	import PageHeader from '$components/PageHeader.svelte';
+	import TextFilterWrapper from '$components/TextFilterWrapper.svelte';
 </script>
 
 <PageHeader>
 	<H1>Turning ideas into experiences with tech.</H1>
 
-	<button
-		class="px-5 py-2 mt-5 transition-colors duration-300 border rounded-full sm:py-4 sm:text-xl hover:bg-gray-600 hover:text-foreground border-tertiary"
-		>More about us</button
+	<a
+		href="#section-1"
+		class="px-5 inline-block py-2 mt-5 transition-all duration-300 border rounded-full sm:py-4 sm:text-xl hover:opacity-40 border-tertiary"
+		>More about us</a
 	>
 </PageHeader>
 
-<!-- <section class="mt-[100vh] flex">
-	<div class="max-w-[50%] grid justify-items-end">
-		<h2 class="text-xl font-medium">
-			Small town, <span>big ideas</span>
-			<svg class="hide-svg"
-				><filter xmlns="http://www.w3.org/2000/svg" id="refract_32628-335"
-					><feTurbulence baseFrequency="0.04" seed="96" /><feDisplacementMap
-						in="SourceGraphic"
-						in2="map"
-						scale="4"
-						xChannelSelector="R"
-						yChannelSelector="G"
-					/></filter
-				></svg
-			>.
-		</h2>
+<BackgroundSection
+	sectionId="section-1"
+	backgroundUrl="https://assets.honeybankstudios.com/town-drone-shot.jpg"
+>
+	<H2>
+		Small town, <TextFilterWrapper>big ideas</TextFilterWrapper> .
+	</H2>
 
-		<p>
-			We're Honeybank Studios, based in simple St Leonards, a cosy British town on the coast. But
-			our tech? It's big and it's global, and it's seen by millions.
-		</p>
-		<p>
-			But we like to keep it simple, build awesome tech that scales, while staying true to our core.
-			We'll bring you the cutting-edge solutions that make a real difference, wherever you are in
-			the world.
-		</p>
-	</div>
+	<p>
+		We're Honeybank Studios, based in St Leonards, a cosy British town on the coast. But our tech?
+		It's big and it's global, and it's seen by millions.
+	</p>
+	<p>
+		But we like to keep it simple, build awesome tech that scales, while staying true to our core.
+		We'll bring you the cutting-edge solutions that make a real difference, wherever you are in the
+		world.
+	</p>
+</BackgroundSection>
 
-	<aside />
-</section>
+<BackgroundSection
+	backgroundUrl="https://assets.honeybankstudios.com/flipped/man-against-audio-equip.webp"
+>
+	<H2>
+		We are committed to building sleek and <TextFilterWrapper>simple solutions</TextFilterWrapper> that
+		<TextFilterWrapper>solve problems</TextFilterWrapper> for our customers.
+	</H2>
 
-<style>
-	.clip-header::before {
-		content: '';
-		position: absolute;
-		inset-block-start: 0;
-		inset-inline-start: 0;
-		inset-inline-end: 0;
-		inset-block-end: 0;
-		background-color: #a13f87;
-		clip-path: polygon(400px 0%, 100% 0%, 100% 100%, 0px 100%, 0 600px);
-	}
+	<p>
+		We believe in creating products that are intuitive, efficient, and easy to use—and we believe
+		that those products can be built by people who care about their customers as much as they do
+		about their code quality.
+	</p>
+</BackgroundSection>
 
-	@media (width > 629px) {
-		.clip-header::before {
-			clip-path: polygon(800px 0%, 100% 0%, 100% 100%, 0px 100%, 0 1200px);
-		}
-	}
+<BackgroundSection backgroundUrl="https://assets.honeybankstudios.com/loft-office.jpg">
+	<H2 class="homepage-h2">
+		We started Honeybank Studios because we wanted to build offerings that are better than
+		<TextFilterWrapper>anything</TextFilterWrapper> anything on the market.
+	</H2>
 
+	<p>
+		We dislike old systems with old ideas, so we started from scratch with new ideas to change the
+		way we live, with a global team of professionals.
+	</p>
 
-    aside {
-		--clip-start-percentage: 50%;
-		--clip-end-percentage: 0%;
-		height: 100px;
-		right: 0;
-		top: 0;
-		bottom: 0;
-		width: 50%;
-		-o-object-fit: cover;
-		object-fit: cover;
-		z-index: 0;
-		background-color: #a13f87;
-		mask-image: linear-gradient(90deg, rgba(0, 0, 0, 0) 0%, rgb(0, 0, 0) 80%);
-		clip-path: polygon(
-			var(--clip-start-percentage) 0%,
-			100% 0%,
-			100% 100%,
-			var(--clip-end-percentage) 100%
-		);
-		/* clip-path: polygon(400px 0%, 100% 0%, 100% 100%, 0px 100%, 0 600px); */
-	}
-
-	h2 span {
-		display: inline-block;
-		position: relative;
-		-webkit-user-select: text;
-		-moz-user-select: text;
-		user-select: text;
-		white-space: nowrap;
-
-		&::before {
-			content: '';
-			position: absolute;
-			left: -0.125rem;
-			right: -0.125rem;
-			bottom: 0;
-			top: 0;
-			background: red;
-			opacity: 0.8;
-			mix-blend-mode: screen;
-			transform: skew(10deg);
-			filter: url(#refract_32628-335);
-		}
-	}
-
-	.hide-svg {
-		position: fixed;
-		width: 0;
-		height: 0;
-		pointer-events: none;
-		left: 0;
-		top: 0;
-	}
-
-    </style> -->
+	<p>
+		So why not contact us to see how we can transform your everyday tech into a next generation
+		experience?
+	</p>
+</BackgroundSection>
