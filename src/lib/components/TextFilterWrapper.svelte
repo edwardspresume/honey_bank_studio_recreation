ca53453d2444660bba043c4ee4abff53997ff9a7
Edwards Presume <edwardspresume@gmail.com>
2023-10-27 14:52:32 -0400

    feat(TextFilterWrapper.svelte): add TextFilterWrapper component to provide a text filter effect on the wrapped content

---
+++ src/lib/components/TextFilterWrapper.svelte
@@ -0,0 +1,19 @@
+<span
+	class="relative inline-block before:absolute before:left-[-0.125rem] before:right-[-0.125rem] before:bottom-0 before:top-0 before:mix-blend-screen before:bg-tertiary before:skew-x-12 before:opacity-80 filter- before:[filter:url(#refract_32628-336)]"
+>
+	<slot />
+</span>
+
+<svg class="fixed top-0 left-0 w-0 h-0 pointer-events-none">
+	<filter xmlns="http://www.w3.org/2000/svg" id="refract_32628-336">
+		<feTurbulence baseFrequency="0.04" seed="96" />
+
+		<feDisplacementMap
+			in="SourceGraphic"
+			in2="map"
+			scale="4"
+			xChannelSelector="R"
+			yChannelSelector="G"
+		/>
+	</filter>
+</svg>
